e1322738e5db8d8cb11321dc0669e24a9e6dcedc
Waqas Khan <71628657+Waqaskhan404@users.noreply.github.com>
2023-01-15 23:12:40 -0500

    first Commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Invoice from "./Invoice/Invoice"
+export default function App() {
+
+  return (
+    <Invoice/>
+  )
+}
---
+++ src/Invoice/Invoice.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Apps from '../components/App'
+export default function Invoice() {
+  return (
+    <div>
+        <Apps/>
+    </div>
+  )
+}
